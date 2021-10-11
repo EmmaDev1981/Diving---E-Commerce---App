7256b9f8b028066536c65d6f625c1fd57156def8
Emmanuel <emanuelnavas@hotmail.com>
2021-10-11 17:58:23 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
-import Homepage from './pages/homepage/homepage.component.jsx';
 import { BrowserRouter, Switch, Route} from 'react-router-dom'
+
+import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/homepage/shop/shop.components';
+import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route  path='/shop' component={ShopPage} />
+        <Route  path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </BrowserRouter>
   );
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,10 +2,25 @@ import React from "react";
 import { Link } from 'react-router-dom'
 import './header.styles.scss'
 
+import { ReactComponent as Logo} from '../../assets/crown.svg'
+
 const Header = () => {
     return (
         <div className="header">
-
+            <Link className="logo-container" to="/">
+                <Logo className="logo"/>
+            </Link>
+            <div className="options">
+                <Link className="option" to='/shop'>
+                    SHOP
+                </Link>
+                <Link className="option" to='/shop'>
+                    CONTACT
+                </Link>
+                <Link className="option" to='/signin'>
+                    SIGN IN
+                </Link>
+            </div>
         </div>
     )
 }
