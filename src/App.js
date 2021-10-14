aa42914dd51d998c8717191fbec2bb36492c8d0d
Emmanuel <emanuelnavas@hotmail.com>
2021-10-14 10:24:04 -0300

    update redux

--- src/App.js
+++ src/App.js
@@ -1,43 +1,56 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import { BrowserRouter, Switch, Route} from 'react-router-dom'
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/homepage/shop/shop.components';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 
-class App extends Component {
-
+class App extends React.Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
       currentUser: null
-    }
+    };
   }
 
-  unsubscribeFfromAuth = null;
+  unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFfromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user})
-    })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
+    });
   }
 
-  componentWillUnmount () {
-    this.unsubscribeFfromAuth()
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
   }
 
   render() {
     return (
       <BrowserRouter>
-        <Header currentUser={this.state.currentUser}/>
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path='/' component={Homepage} />
-          <Route  path='/shop' component={ShopPage} />
-          <Route  path='/signin' component={SignInAndSignUpPage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
         </Switch>
       </BrowserRouter>
     );
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import './sign-in.styles.scss';
 import FormInput from "../form-input/form-input.component";
 import CustomButton from '../custom-button/custom-button.component'
 
-import { signInWithGoogle } from '../../firebase/firebase.utils'
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils'
 
 class SignIn extends React.Component {
     constructor() {
@@ -16,9 +16,19 @@ class SignIn extends React.Component {
 
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault()
+
+       const { email, password} = this.state;
+
+      try {
+        await auth.signInWithEmailAndPassword(email, password)
         this.setState({email: '', password: ''})
+        
+      }catch (error) {
+          console.log(error)
+      }
+
     }
 
     handleChange = event => {
@@ -49,7 +59,7 @@ class SignIn extends React.Component {
                 required
               />
               <CustomButton type="submit"> SIGN IN </CustomButton>
-              <CustomButton onClick={signInWithGoogle}> SIGN IN WITH GOOGLE </CustomButton>
+              <CustomButton onClick={signInWithGoogle} isGoogleSignin> SIGN IN WITH GOOGLE </CustomButton>
             </form>
           </div>
         );
