f5124c4f4a6e15b18ade4be1fa7562c7c367c77a
Emmanuel <emanuelnavas@hotmail.com>
2023-07-11 13:30:52 -0300

    npm issues fix

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,7 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
-import 'firebase/compat/auth';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
 
 const config = {
     
